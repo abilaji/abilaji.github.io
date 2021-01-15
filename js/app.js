ae771f6e4e86d8b295189f3d93a5a8d13efc6d92
unknown <abilaji96@gmail.com>
2021-01-15 20:28:53 +0700

    index.html

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const sr = ScrollReveal();
+
+sr.reveal(".content",{
+    origin:"left",
+    distance:"50px",
+    duration:3000,
+    reset:true
+});
+
+sr.reveal(".foto",{
+    origin:"right",
+    distance:"50px",
+    duration: 3000,
+    reset:true
+});
+
+sr.reveal(".imgAbout",{
+    origin:"bottom",
+    distance:"50px",
+    duration:3000,
+    reset:true
+});
+
+sr.reveal(".about02",{
+    origin:"right",
+    distance:"50px",
+    duration:3000,
+    reset:true
+});
+
+sr.reveal(".thumbnail",{
+    origin:"bottom",
+    distance:"50px",
+    duration:3000,
+    reset:true
+});
